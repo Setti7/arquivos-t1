7982ad469514f4dfeddb2e24b578534987a89fa2
Leonardo Chieppe <chieppe2142@gmail.com>
2020-07-10 16:46:09 -0300

    Arvore B iniciada
    arvore B foi iniciada

---
+++ src/arvoreB.h
@@ -0,0 +1,26 @@
+#ifndef ARVOREB_H_INCLUDED
+#define ARVOREB_H_INCLUDED
+
+#include <stdio.h>
+
+#define MAX_KEYS 5  //numero de chaves por pagina definido pela especificacao
+
+struct arvoreBPagina {
+    int nivel;                  //informa o nivel de profundidade da pagina
+    int numeroDeChaves;         //contador de ocupacao da pagina
+    int chaves[MAX_KEYS];       //as chaves
+    int RRNChaves[MAX_KEYS];    //RRN dos campos no arquivo de dados
+    int filhos[MAX_KEYS+1];     //RRN das paginas filhas
+} Pagina;
+
+struct arvoreBCabecalho {
+    char status;        //informa a consistencia do arquivo de indices
+    int raiz;           //informa o RRN do no raiz
+    int profundidade;   //numero de niveis da arvore
+    int proxRRN;        //informa qual a proxima pagina livre para inserir elementos na arvore
+    int numeroChaves;   //informa quantas chaves estao armazenadas no arquivo
+    char lixo[55];      //lixo para preencher o valor de 72 bytes do cabecalho
+} Cabecalho;
+
+
+#endif // ARVOREB_H_INCLUDED
